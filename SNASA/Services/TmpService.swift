651ec535cb35c9af79a2c2cc70fa161014fc9037
Nebo <kindersmail08@gmail.com>
2023-07-08 19:14:54 +0300

    added tmp service

--- SNASA/Services/TmpService.swift
+++ SNASA/Services/TmpService.swift
@@ -10,11 +10,15 @@ import Foundation
 class TmpService {
     
     func loadDate(completion: @escaping (Date) -> Void) {
-        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            completion(Date())
+        }
     }
     
     func loadTwo(completion: @escaping (Int) -> Void) {
-        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            completion(2)
+        }
     }
     
     func loadImage(completion: @escaping (String) -> Void) {
