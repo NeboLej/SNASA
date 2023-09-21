c0def23ab09ab9d7e796301b3687619f62467caa
Anton Pakhomov <apahomov@kolos.studio>
2023-09-21 15:18:41 +0300

    refactor card component, added colors + fonts

---
+++ SNASA/Resources/Font+.swift
@@ -0,0 +1,18 @@
+//
+//  Font+.swift
+//  SNASA
+//
+//  Created by Nebo on 21.09.2023.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    struct MyFont {
+        static let standartRegular = "OldStandardTT-Regular"
+        static let standartBold = "OldStandardTT-Bold"
+        static let standartItalic = "OldStandardTT-Italic"
+        static let readTextBold = "iowanoldstyle_bold"
+    }
+}
